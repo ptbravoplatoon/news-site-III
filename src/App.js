7842c8e4c8b319d1e8b9c2b51ed79beace5de7dc
johnlillge <81765408+johnlillge@users.noreply.github.com>
2021-08-07 16:50:44 -0500

    changed App.js to functional

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import AppNav from './components/AppNav/AppNav.js';
 import HomePage from './pages/HomePage.js';
 import ArticlePage from './pages/ArticlePage.js';
 
-class App extends Component {
-	render() {
-		return (
-			<div>
-				<AppNav handleNavClick={(clickedItem) => console.log(clickedItem)} />
-				<Router>
-					<div>
-						<Route exact path="/" component={HomePage} />
-						<Route exact path="/articles/:articleID" component={ArticlePage} />
-					</div>
-				</Router>
-			</div>
-		);
-	}
+function App() {
+	return (
+		<div>
+			<AppNav handleNavClick={(clickedItem) => console.log(clickedItem)} />
+			<Router>
+				<div>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/articles/:articleID" component={ArticlePage} />
+				</div>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
-
-// Functional solution
-// function App() {
-//   const [ navItems, setNavItems ] = useState(navItems);
-
-//   return (
-//     <div>
-//       <AppNav navItems={navItems} handleNavClick={(clickedItem) => console.log(clickedItem)} />
-//       <Router>
-//         <div>
-//           <Route exact path="/" component={HomePage} />
-//           <Route exact path="/articles/:articleID" component={ArticlePage} />
-//         </div>
-//       </Router>
-//     </div>
-//   );
-// }
