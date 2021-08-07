d8bd33d093964f59adbfd401cd7712e817289e07
adamziolk <adam.lawrence.ziolkowski@gmail.com>
2021-08-07 11:59:04 -0600

    news-site-III

--- src/api/ArticlesAPI.js
+++ src/api/ArticlesAPI.js
@@ -1,10 +1,26 @@
-const fetchArticleByID = (articleID) => {
+const fetchArticleByID = async (articleID) => {
+  let response =  await fetch(`http://localhost:3001/api/articles/${articleID}`)
+  let data = await response.json()
+  return data
 };
 
-const fetchArticlesBySection = (section) => {
+const fetchArticlesBySection = async (section) => {
+
+  let response = await fetch(`http://localhost:3001/api/articles?filter={"where":{"section":"${section}"}}`)
+  let data = await response.json()
+  return data
 };
 
-const fetchArticles = (filters = null) => {
+const fetchArticles = async (filters = null) => {
+  if (filters) {
+    let response = await fetch(`http://localhost:3001/api/articles?filter={"where":${filters}}`)
+    let data = await response.json()
+    return data
+  } else {
+    let response = await fetch(`http://localhost:3001/api/articles`)
+    let data = await response.json()
+    return data
+  }
 };
 
 export default {
