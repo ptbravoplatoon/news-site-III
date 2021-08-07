da237a362f0e2f4b38aa6a855901dd3747538bc1
johnlillge <81765408+johnlillge@users.noreply.github.com>
2021-08-07 15:50:02 -0500

    class based solution

--- src/api/ArticlesAPI.js
+++ src/api/ArticlesAPI.js
@@ -1,14 +1,37 @@
-const fetchArticleByID = (articleID) => {
+const fetchArticleByID = async (articleID) => {
+	try {
+		const response = await fetch(`http://localhost:3001/api/articles/${articleID}`);
+		const responseBody = await response.json();
+		return responseBody;
+	} catch (error) {
+		throw new Error('Call failed');
+	}
 };
 
-const fetchArticlesBySection = (section) => {
+const fetchArticlesBySection = async (section) => {
+	try {
+		const response = await fetch(`http://localhost:3001/api/articles?filter={"where":{"section":${section}}}`);
+		const responseBody = await response.json();
+		return responseBody;
+	} catch (error) {
+		throw new Error('Call failed');
+	}
 };
 
-const fetchArticles = (filters = null) => {
+const fetchArticles = async (filters = null) => {
+	try {
+		if (!filters) {
+			const response = await fetch('http://localhost:3001/api/articles');
+			const responseBody = await response.json();
+			return responseBody;
+		} else {
+			const response = await fetch(`http://localhost:3001/api/articles?filter=${filters}`);
+			const responseBody = await response.json();
+			return responseBody;
+		}
+	} catch (error) {
+		throw new Error('Call failed');
+	}
 };
 
-export default {
-  fetchArticleByID,
-  fetchArticles,
-  fetchArticlesBySection
-};
+export { fetchArticleByID, fetchArticles, fetchArticlesBySection };
--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import { Navbar } from 'reactstrap';
+import navItems from '../../config/Sections.json';
 
 class AppNav extends Component {
-  render() {
-    const { navItems, handleNavClick } = this.props;
+	render() {
+		const { handleNavClick } = this.props;
 
-    return (
-      <Navbar color="light">
-        {navItems.map((navItem) =>
-          <a href="#" onClick={ () => handleNavClick( navItem.value )} >
-            { navItem.label } |
-          </a>
-        )}
-      </Navbar>
-    )
-  }
+		return (
+			<Navbar color="light">
+				{navItems.map((section, i) => (
+					<a href="#" key={`navItem-${i}`} onClick={() => handleNavClick(section.value)}>
+						{section.label} |
+					</a>
+				))}
+			</Navbar>
+		);
+	}
 }
 
 export default AppNav;
-
 
 // Functional solution:
 // function AppNav({ navItems, handleNavClick }) {
